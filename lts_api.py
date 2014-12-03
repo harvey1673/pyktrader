cf777e36de20cdfd4ed976af732d877b13beddea
harveywwu <harvey_wwu@hotmail.com>
2014-12-03 19:00:36 +0800

    Update lts_api.py

--- lts_api.py
+++ lts_api.py
@@ -1,10 +1,11 @@
-﻿#-*- coding:utf-8 -*-
+#-*- coding:utf-8 -*-
 import ctp.lts
 from agent import *
 from base import *
 from misc import *
 import logging
 import order
+
 
 class LtsMdSpi(CTPMdMixin, ctp.lts.MdApi):
     '''
@@ -15,14 +16,12 @@ class LtsMdSpi(CTPMdMixin, ctp.lts.MdApi):
     ApiStruct = ctp.lts.ApiStruct
     def __init__(self,
             instruments, #合约映射 name ==>c_instrument
-            exchanges,
             broker_id,   #期货公司ID
             investor_id, #投资者ID
             passwd, #口令
             agent,  #实际操作对象
         ):            
         self.instruments = instruments
-        self.exchanges = exchanges
         self.broker_id =broker_id
         self.investor_id = investor_id
         self.passwd = passwd
@@ -35,24 +34,32 @@ class LtsMdSpi(CTPMdMixin, ctp.lts.MdApi):
         pass
 
     def subscribe_market_data(self, instruments):
-        instIds = {'SSE': [], 'SZE':[]}
-        for inst in instruments:
-            instId, echgId = inst.split('.')
-            instIds[echgId].append(instId)
-        for echgId in instIds:
-            if len(instIds[echgId])>0:
-                self.SubscribeMarketData(instIds[echgId], echgId)
+		inst_set = set(instruments)
+		for exch in CHN_Stock_Exch:
+			insts = list(inst_set & set(CHN_Stock_Exch[exch]))
+            if len(insts)>0:
+                self.SubscribeMarketData(insts, exch)
 
     def market_data2tick(self, dp, timestamp):
         print dp
         #market_data的格式转换和整理, 交易数据都转换为整数
         try:
             #rev的后四个字段在模拟行情中经常出错
-            rev = TickData(instID=dp.InstrumentID, timestamp=timestamp, openInterest=dp.OpenInterest, 
-                           volume=dp.Volume, price=dp.LastPrice, 
+            rev = StockTick(instID=dp.InstrumentID, timestamp=timestamp, openInterest=dp.OpenInterest, 
+                           volume=dp.Volume, turnover=dp.Turnover, price=dp.LastPrice, 
+						   open=dp.OpenPrice, close=dp.ClosePrice,
                            high=dp.HighestPrice, low=dp.LowestPrice, 
                            bidPrice1=dp.BidPrice1, bidVol1=dp.BidVolume1, 
-                           askPrice1=dp.AskPrice1, askVol1=dp.AskVolume1)
+                           askPrice1=dp.AskPrice1, askVol1=dp.AskVolume1,
+                           bidPrice1=dp.BidPrice2, bidVol1=dp.BidVolume2, 
+                           askPrice1=dp.AskPrice2, askVol1=dp.AskVolume2,
+                           bidPrice1=dp.BidPrice3, bidVol1=dp.BidVolume3, 
+                           askPrice1=dp.AskPrice3, askVol1=dp.AskVolume3,
+                           bidPrice1=dp.BidPrice4, bidVol1=dp.BidVolume4, 
+                           askPrice1=dp.AskPrice4, askVol1=dp.AskVolume4,
+                           bidPrice1=dp.BidPrice5, bidVol1=dp.BidVolume5, 
+                           askPrice1=dp.AskPrice5, askVol1=dp.AskVolume5 )
+
         except Exception,inst:
             self.logger.warning(u'MD:%s 行情数据转换错误:updateTime="%s",msec="%s",tday="%s"' % (dp.InstrumentID, timestamp))
         return rev
@@ -81,77 +88,9 @@ class LtsTraderSpi(CTPTraderQryMixin, CTPTraderRspMixin, ctp.lts.TraderApi):
         self.ctp_orders = {}
         self.ctp_trades = {}
                 
-    def OnRspUserLogin(self, pRspUserLogin, pRspInfo, nRequestID, bIsLast):
-        CTPTraderRspMixin.OnRspUserLogin(self, pRspUserLogin, pRspInfo, nRequestID, bIsLast)
-        self.confirm_settlement_info()
-
-    def query_settlement_confirm(self):
-        '''
-            这个基本没用，不如直接确认
-            而且需要进一步明确：有史以来第一次确认前查询确认情况还是每天第一次确认查询情况时，返回的响应中
-                pSettlementInfoConfirm为空指针. 如果是后者,则建议每日不查询确认情况,或者在generate_struct中对
-                CThostFtdcSettlementInfoConfirmField的new_函数进行特殊处理
-            CTP写代码的这帮家伙素质太差了，边界条件也不测试，后置断言也不整，空指针乱飞
-            2011-3-1 确认每天未确认前查询确认情况时,返回的响应中pSettlementInfoConfirm为空指针
-            并且妥善处理空指针之后,仍然有问题,在其中查询结算单无动静
-        '''
-        req = self.ApiStruct.QrySettlementInfoConfirm(BrokerID=self.broker_id,InvestorID=self.investor_id)
-        self.ReqQrySettlementInfoConfirm(req,self.agent.inc_request_id())
-
-    def query_settlement_info(self):
-        #不填日期表示取上一天结算单,并在响应函数中确认
-        self.logger.info(u'TD:取上一日结算单信息并确认,BrokerID=%s,investorID=%s' % (self.broker_id,self.investor_id))
-        req = self.ApiStruct.QrySettlementInfo(BrokerID=self.broker_id,InvestorID=self.investor_id,TradingDay=u'')
-        #print req.BrokerID,req.InvestorID,req.TradingDay
-        time.sleep(1)
-        self.ReqQrySettlementInfo(req,self.agent.inc_request_id())
-
-    def confirm_settlement_info(self):
-        self.logger.info(u'TD-CSI:准备确认结算单')
-        req = self.ApiStruct.SettlementInfoConfirm(BrokerID=self.broker_id,InvestorID=self.investor_id)
-        self.ReqSettlementInfoConfirm(req,self.agent.inc_request_id())
-
-    def OnRspQrySettlementInfo(self, pSettlementInfo, pRspInfo, nRequestID, bIsLast):
-        '''请求查询投资者结算信息响应'''
-        print u'Rsp 结算单查询'
-        if(self.resp_common(pRspInfo,bIsLast,u'结算单查询')>0):
-            self.logger.info(u'结算单查询完成,准备确认')
-            try:
-                self.logger.info(u'TD:结算单内容:%s' % pSettlementInfo.Content)
-            except Exception,inst:
-                self.logger.warning(u'TD-ORQSI-A 结算单内容错误:%s' % str(inst))
-            self.confirm_settlement_info()
-        else:  #这里是未完成分支,需要直接忽略
-            try:
-                self.logger.info(u'TD:结算单接收中...:%s' % pSettlementInfo.Content)
-            except Exception,inst:
-                self.logger.warning(u'TD-ORQSI-B 结算单内容错误:%s' % str(inst))
-            #self.agent.initialize()
-            pass
-
-    def OnRspQrySettlementInfoConfirm(self, pSettlementInfoConfirm, pRspInfo, nRequestID, bIsLast):
-        '''请求查询结算信息确认响应'''
-        self.logger.debug(u"TD:结算单确认信息查询响应:rspInfo=%s,结算单确认=%s" % (pRspInfo,pSettlementInfoConfirm))
-        #self.query_settlement_info()
-        if(self.resp_common(pRspInfo,bIsLast,u'结算单确认情况查询')>0):
-            if(pSettlementInfoConfirm == None or int(pSettlementInfoConfirm.ConfirmDate) < int(self.agent.scur_day.strftime('%Y%m%d'))):
-                #其实这个判断是不对的，如果周日对周五的结果进行了确认，那么周一实际上已经不需要再次确认了
-                if(pSettlementInfoConfirm != None):
-                    self.logger.info(u'TD:最新结算单未确认，需查询后再确认,最后确认时间=%s,scur_day:%s' % (pSettlementInfoConfirm.ConfirmDate,self.agent.scur_day))
-                else:
-                    self.logger.info(u'TD:结算单确认结果为None')
-                self.query_settlement_info()
-            else:
-                self.agent.isSettlementInfoConfirmed = True
-                self.logger.info(u'TD:最新结算单已确认，不需再次确认,最后确认时间=%s,scur_day:%s' % (pSettlementInfoConfirm.ConfirmDate,self.agent.scur_day))
-                self.agent.initialize()
-
-    def OnRspSettlementInfoConfirm(self, pSettlementInfoConfirm, pRspInfo, nRequestID, bIsLast):
-        '''投资者结算结果确认响应'''
-        if(self.resp_common(pRspInfo,bIsLast,u'结算单确认')>0):
-            self.agent.isSettlementInfoConfirmed = True
-            self.logger.info(u'TD:结算单确认时间: %s-%s' %(pSettlementInfoConfirm.ConfirmDate,pSettlementInfoConfirm.ConfirmTime))
-        self.agent.initialize()
+#    def OnRspUserLogin(self, pRspUserLogin, pRspInfo, nRequestID, bIsLast):
+#        CTPTraderRspMixin.OnRspUserLogin(self, pRspUserLogin, pRspInfo, nRequestID, bIsLast)
+#        self.confirm_settlement_info()
         
     def check_order_status(self):
         Is_Set = False
@@ -219,7 +158,7 @@ def create_agent(agent_name, usercfg, tradercfg, insts, strat_cfg, tday = dateti
 def test_main():
     logging.basicConfig(filename="save_lts_agent.log",level=logging.INFO,format='%(name)s:%(funcName)s:%(lineno)d:%(asctime)s %(levelname)s %(message)s')
     app_name = 'SaveAgent'
-    insts = ['600104.SSE', '000300.SSE', '510180.SSE']
+    insts = ['600104', '000300', '510180']
     my_agent = SaveAgent(name = app_name, trader = None, cuser = None, instruments=insts, daily_data_days=0, min_data_days=0)
     my_agent.save_flag = False
     make_user(my_agent,LTS_SO_USER, insts)
