ee2c9f13986c3dbcbf647c2132da12a4927a7765
harveywwu <harvey_wwu@hotmail.com>
2014-12-16 16:02:59 +0800

    Update strat_turtle.py

--- strat_turtle.py
+++ strat_turtle.py
@@ -14,16 +14,16 @@ class TurtleTrader(Strategy):
                 ('d', BaseObject(name = 'DONCH_L10', sfunc=fcustom(data_handler.DONCH_L, n=10), rfunc=fcustom(data_handler.donch_l, n=10))),\
                 ('d', BaseObject(name = 'DONCH_H10', sfunc=fcustom(data_handler.DONCH_H, n=10), rfunc=fcustom(data_handler.donch_h, n=10))),\
                 ('d', BaseObject(name = 'DONCH_L20', sfunc=fcustom(data_handler.DONCH_L, n=20), rfunc=fcustom(data_handler.donch_l, n=20))),\
-                ('d', BaseObject(name = 'DONCH_H20', sfunc=fcustom(data_handler.DONCH_H, n=20), rfunc=fcustom(data_handler.donch_h, n=10))),\
-                ('d', BaseObject(name = 'DONCH_L55', sfunc=fcustom(data_handler.DONCH_L, n=55), rfunc=fcustom(data_handler.donch_l, n=10))),\
+                ('d', BaseObject(name = 'DONCH_H20', sfunc=fcustom(data_handler.DONCH_H, n=20), rfunc=fcustom(data_handler.donch_h, n=20))),\
+                ('d', BaseObject(name = 'DONCH_L55', sfunc=fcustom(data_handler.DONCH_L, n=55), rfunc=fcustom(data_handler.donch_l, n=55))),\
                 ('d', BaseObject(name = 'DONCH_H55', sfunc=fcustom(data_handler.DONCH_H, n=55), rfunc=fcustom(data_handler.donch_h, n=55)))]    
         self.capital = capital 
         self.pos_ratio = 0.01
         self.stop_loss = 2.0
+		self.order_type = OPT_LIMIT_ORDER
     
     def run_tick(self, ctick):
         inst = ctick.instID
-        under = [inst]
         df = self.agent.day_data[inst]
         cur_atr = df.ix[-1,'ATR_20']
         hh = [df.ix[-1,'DONCH_H20'],df.ix[-1,'DONCH_H10']]
@@ -66,7 +66,6 @@ class TurtleTrader(Strategy):
         self.submitted_pos[idx] = [etrade for etrade in self.submitted_pos[idx] if etrade.status!=order.ETradeStatus.StratConfirm]
         cur_price = (ctick.askPrice1 + ctick.bidPrice1)/2.0
         if len(self.submitted_pos[idx]) == 0:
-            #寮�粨 
             if len(self.positions[inst]) == 0: 
                 buysell = 0
                 if (cur_price > hh[0]):
@@ -76,38 +75,38 @@ class TurtleTrader(Strategy):
                 if buysell !=0:
                     valid_time = self.agent.tick_id + 600
                     etrade = order.ETrade( [inst], [self.trade_unit[idx][0]*buysell], \
-                                           [OPT_LIMIT_ORDER], cur_price, [5],  \
+                                           [self.order_type], cur_price, [5],  \
                                            valid_time, self.name, self.agent.name)
                     tradepos = TradePos([inst], self.trade_unit[idx], buysell, \
                                         cur_price, cur_price - cur_atr*self.stop_loss*buysell)
-                    tradepos.set_tradeid(etrade.id, buysell)
+                    tradepos.entry_tradeid = etrade.id
                     self.submitted_pos[idx].append(etrade)
                     self.positions[inst].append(tradepos)
                     self.agent.submit_trade(etrade)
                     return 1
-            #鍔犱粨鎴栨竻浠�
             else:
                 buysell = self.positions[idx][0].direction
-                #娓呬粨1
                 units = len(self.positions[idx])
                 for tradepos in self.positions[idx]:
                     if (cur_price < ll[1] and buysell == 1) or (cur_price > hh[1] and buysell == -1) \
                         or ((cur_price - tradepos.exit_target)*buysell < 0):
                         valid_time = self.agent.tick_id + 600
                         etrade = order.ETrade( [inst], [-self.trade_unit[idx][0]*buysell], \
-                                               [OPT_LIMIT_ORDER], cur_price, [5], \
+                                               [self.order_type], cur_price, [5], \
                                                valid_time, self.name, self.agent.name)
-                        tradepos.set_tradeid(etrade.id, -buysell)
+                        tradepos.exit_tradeid = etrade.id
                         self.submitted_pos[idx].append(etrade)
                         self.agent.submit_trade(etrade)
                     elif  units < 4 and (cur_price - self.positions[idx][-1].entry_price)*buysell >= cur_atr/2.0:
+						for pos in self.positions[idx]:
+							pos.exit_target = cur_price - cur_atr*self.stop_loss*buysell
                         valid_time = self.agent.tick_id + 600
                         etrade = order.ETrade( [inst], [self.trade_unit[idx][0]*buysell], \
-                                               [OPT_LIMIT_ORDER], cur_price, [5],  \
+                                               [self.order_type], cur_price, [5],  \
                                                valid_time, self.name, self.agent.name)
                         tradepos = TradePos([inst], self.trade_unit[idx], buysell, \
                                             cur_price, cur_price - cur_atr*self.stop_loss*buysell)
-                        tradepos.set_tradeid(etrade.id, buysell)
+                        tradepos.entry_tradeid = etrade.id
                         self.submitted_pos[idx].append(etrade)
                         self.positions[inst].append(tradepos)
                         self.agent.submit_trade(etrade)                  
