ddf16afc55b6d43b2b6a2f50b69a2fe82022fe3b
harveywwu <harvey_wwu@hotmail.com>
2014-12-23 18:36:55 +0800

    Update strat_dual_thrust.py

--- strat_dual_thrust.py
+++ strat_dual_thrust.py
@@ -38,96 +38,46 @@ class DTTrader(Strategy):
         buy_trig = tday_open + self.ratios[idx][0] * cur_rng
         sell_trig = tday_open - self.ratio[idx][1] * cur_rng
         self.update_positions(idx)
-        ask = ctick.askPrice1 
-        bid = ctick.bidPrice1
-        if (tick_id >= self.last_tick_id) and (self.close_tday[idx]):
-            if len(self.positions[idx])>0:
-                curr_pos = self.positions[idx][0]
-                buysell = curr_pos.direction
+        curr_price = (ctick.askPrice1 + ctick.bidPrice1)/2.0
+		curr_pos = None
+		buysell = 0
+		if len(self.positions[idx])>0:
+			curr_pos = self.positions[idx][0]
+			buysell = curr_pos.direction
+        if (tick_id >= self.last_tick_id):
+            if (buysell!=0) and (self.close_tday[idx]):
                 valid_time = self.agent.tick_id + 600
                 etrade = order.ETrade( [inst], [-self.trade_unit[idx][0]*buysell], \
-                                               [self.order_type], (ask+bid)/2, [5], \
+                                               [self.order_type], -curr_price*buysell, [5], \
                                                valid_time, self.name, self.agent.name)
                 curr_pos.exit_tradeid = etrade.id
                 self.submitted_pos[idx].append(etrade)
                 self.agent.submit_trade(etrade)
         elif len(self.submitted_pos[idx]) == 0:
-            direction = 0
-            if len(self.positions[idx])>0:
-                direction = self.positions[inst][0].direction
-            if (ask > buy_trig) and (direction <=0):
-                if direction != 0:
-
-                if len(curr_pos) > 0:
-                    curr_pos[0].close(mslice.close+offset, dd)
-                    tradeid += 1
-                    curr_pos[0].exit_tradeid = tradeid
-                    closed_trades.append(curr_pos[0])
-                    curr_pos = []
-                    mdf.ix[dd, 'cost'] -=  abs(pos) * (offset + mslice.close*tcost)
-                new_pos = strat.TradePos([mslice.contract], [1], unit, buytrig, 0)
-                tradeid += 1
-                new_pos.entry_tradeid = tradeid
-                new_pos.open(mslice.close + offset, dd)
-                curr_pos.append(new_pos)
-                pos = unit
-                mdf.ix[dd, 'cost'] -=  abs(pos) * (offset + mslice.close*tcost)
-            elif (mslice.close <= selltrig) and (pos >=0 ):
-                if len(curr_pos) > 0:
-                    curr_pos[0].close(mslice.close-offset, dd)
-                    tradeid += 1
-                    curr_pos[0].exit_tradeid = tradeid
-                    closed_trades.append(curr_pos[0])
-                    curr_pos = []
-                    mdf.ix[dd, 'cost'] -=  abs(pos) * (offset + mslice.close*tcost)
-                new_pos = strat.TradePos([mslice.contract], [1], -unit, selltrig, 0)
-                tradeid += 1
-                new_pos.entry_tradeid = tradeid
-                new_pos.open(mslice.close - offset, dd)
-                curr_pos.append(new_pos)
-                pos = -unit
-                
-                buysell = 0
-                if (cur_price > hh[0]):
-                    buysell = 1
-                elif (cur_price < ll[0]):
-                    buysell = -1                 
-                if buysell !=0:
-                    valid_time = self.agent.tick_id + 600
-                    etrade = order.ETrade( [inst], [self.trade_unit[idx][0]*buysell], \
-                                           [self.order_type], cur_price, [5],  \
-                                           valid_time, self.name, self.agent.name)
-                    tradepos = TradePos([inst], self.trade_unit[idx], buysell, \
-                                        cur_price, cur_price - cur_atr*self.stop_loss*buysell)
-                    tradepos.entry_tradeid = etrade.id
-                    self.submitted_pos[idx].append(etrade)
-                    self.positions[inst].append(tradepos)
-                    self.agent.submit_trade(etrade)
-                    return 1
-            else:
-                buysell = self.positions[idx][0].direction
-                units = len(self.positions[idx])
-                for tradepos in self.positions[idx]:
-                    if (cur_price < ll[1] and buysell == 1) or (cur_price > hh[1] and buysell == -1) \
-                        or ((cur_price - tradepos.exit_target)*buysell < 0):
-                        valid_time = self.agent.tick_id + 600
-                        etrade = order.ETrade( [inst], [-self.trade_unit[idx][0]*buysell], \
-                                               [self.order_type], cur_price, [5], \
+            if ((curr_price >= buy_trig) and (buysell <=0)) or ((curr_price <= sell_trig) and (buysell >=0)):
+				if buysell!=0:
+					valid_time = self.agent.tick_id + 600
+					etrade = order.ETrade( [inst], [-self.trade_unit[idx][0]*buysell], \
+                                               [self.order_type], -curr_price*buysell, [5], \
                                                valid_time, self.name, self.agent.name)
-                        tradepos.exit_tradeid = etrade.id
-                        self.submitted_pos[idx].append(etrade)
-                        self.agent.submit_trade(etrade)
-                    elif  units < 4 and (cur_price - self.positions[idx][-1].entry_price)*buysell >= cur_atr/2.0:
-                        for pos in self.positions[idx]:
-                            pos.exit_target = cur_price - cur_atr*self.stop_loss*buysell
-                        valid_time = self.agent.tick_id + 600
-                        etrade = order.ETrade( [inst], [self.trade_unit[idx][0]*buysell], \
-                                               [self.order_type], cur_price, [5],  \
-                                               valid_time, self.name, self.agent.name)
-                        tradepos = TradePos([inst], self.trade_unit[idx], buysell, \
-                                            cur_price, cur_price - cur_atr*self.stop_loss*buysell)
-                        tradepos.entry_tradeid = etrade.id
-                        self.submitted_pos[idx].append(etrade)
-                        self.positions[inst].append(tradepos)
-                        self.agent.submit_trade(etrade)                  
-                    return 1
+					curr_pos.exit_tradeid = etrade.id
+					self.submitted_pos[idx].append(etrade)
+					self.agent.submit_trade(etrade)
+				if  (curr_price >= buy_trig):
+					buysell = 1
+				else:
+					buysell = -1
+				valid_time = self.agent.tick_id + 600
+                etrade = order.ETrade( [inst], [self.trade_unit[idx][0]*buysell], \
+                                       [self.order_type], buysell * cur_price, [3],  \
+                                       valid_time, self.name, self.agent.name)
+                tradepos = TradePos([inst], self.trade_unit[idx], buysell, \
+                                        cur_price, 0)
+                tradepos.entry_tradeid = etrade.id
+                self.submitted_pos[idx].append(etrade)
+                self.positions[inst].append(tradepos)
+                self.agent.submit_trade(etrade)	
+		return 
+		
+    def update_trade_unit(self):
+		pass
