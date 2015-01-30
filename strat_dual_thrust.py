d48246413bcd066a8ed51c743f3feb37524b30fb
harveywwu <harvey_wwu@hotmail.com>
2015-01-30 08:53:33 +0800

    update

--- strat_dual_thrust.py
+++ strat_dual_thrust.py
@@ -8,21 +8,21 @@ import order as order
 import agent
  
 class DTTrader(Strategy):
-    def __init__(self, name, underliers, agent = None, trade_unit = [], ratios = [], stop_loss = [], daily_close = False, email_notify = None):
+    def __init__(self, name, underliers, agent = None, trade_unit = [], ratios = [], lookbacks=[], daily_close = False, email_notify = None):
         Strategy.__init__(self, name, underliers, trade_unit, agent, email_notify)
-        self.data_func = [
-                ('d', BaseObject(name = 'DONCH_H1', sfunc=fcustom(data_handler.DONCH_H, n=1), rfunc=fcustom(data_handler.donch_h, n=1))),\
-                ('d', BaseObject(name = 'DONCH_L1', sfunc=fcustom(data_handler.DONCH_L, n=1), rfunc=fcustom(data_handler.donch_l, n=1))),\
-                ('d', BaseObject(name = 'DONCH_C1', sfunc=fcustom(data_handler.DONCH_C, n=1), rfunc=fcustom(data_handler.donch_c, n=1)))]   
+        self.lookbacks = lookbacks
+        self.data_func = []   
         self.ratios = [(0.7,0.7)]*len(underliers)
         if len(ratios) > 1:
             self.ratio = ratios
         elif len(ratios) == 1: 
             self.ratio = ratios*len(underliers)
+        if len(lookbacks) > 0:
+            self.lookbacks = lookbacks
+        else: 
+            self.lookbacks = [0]*len(underliers)
         self.order_type = OPT_LIMIT_ORDER
-        self.stop_loss = stop_loss
-        self.lookback = 1
-        self.last_tick_id = 2056000
+        self.last_tick_id = 2057000
         self.close_tday = [daily_close]*len(underliers)
 
     def initialize(self):
@@ -33,15 +33,21 @@ class DTTrader(Strategy):
         tick_id = ctick.tick_id
         df = self.agent.day_data[inst]
         price_unit = self.agent.instruments[inst].multiple
-        cur_rng = max(df.ix[-1,'DONCH_H1'] - df.ix[-1,'DONCH_C1'], df.ix[-1,'DONCH_C1'] - df.ix[-1,'DONCH_L1'])
         tday_open = self.agent.cur_day[inst]['open']
         if tday_open <= 0.0:
             print "warning: open price =0.0 for inst=%s" % inst
             return 
-        idx = self.get_index([inst])
+        idx = self.get_index([inst])  
         if idx < 0:
             self.logger.warning('the inst=%s is not in this strategy = %s' % (inst, self.name))
             return
+        win = self.lookbacks[idx]
+        cur_rng = 0.0
+        if win > 0:
+            cur_rng = max(max(df.ix[-win:,'high'])- min(df.ix[-win:,'close']), max(df.ix[-win:,'close']) - min(df.ix[-win:,'low']))
+        else:
+            cur_rng = max(max(df.ix[-2:,'high'])- min(df.ix[-2:,'close']), max(df.ix[-2:,'close']) - min(df.ix[-2:,'low']))
+            cur_rng = max(cur_rng * 0.5, df.ix[-1,'high']-df.ix[-1,'close'],df.ix[-1,'close']-df.ix[-1,'low'])
         buy_trig = tday_open + self.ratios[idx][0] * cur_rng
         sell_trig = tday_open - self.ratios[idx][1] * cur_rng
         save_status = self.update_positions(idx)
--- test_agent.py
+++ test_agent.py
@@ -30,24 +30,25 @@ def test_main(tday, name='test_trade'):
     #trader_cfg = TEST_TRADER
     user_cfg = PROD_USER
     agent_name = name
-    insts_dt = ['m1505', 'RM505', 'y1505', 'p1505', 'a1505', 'l1505', 'pp1505', 'j1505', 'jm1505', 'i1505', 'rb1505', \
+    insts_dt = ['m1505', 'RM505', 'y1505', 'p1505', 'a1505', 'l1505', 'pp1505', 'j1505', 'jm1505', 'i1505', 'rb1505', 'ru1505',\
                 'CF505', 'ME505', 'TA505', 'SR505', 'cu1503', 'al1503', 'zn1503', 'ag1506', 'au1506', 'v1505', 'TC505', \
                 'c1505']
     units_dt = [[1]]*len(insts_dt)
     under_dt = [[inst] for inst in insts_dt]
-    
+    lookbacks_dt = [0]*len(insts_dt)
     insts_turtle = ['m1505', 'RM505', 'y1505', 'p1505', 'a1505', 'l1505', 'pp1505', 'j1505', 'jm1505', 'i1505', 'rb1505', \
                 'CF505', 'ME505', 'TA505', 'SR505', 'cu1503', 'al1503', 'zn1503', 'ag1506', 'au1506', 'v1505', 'TC505', \
                 'c1505', 'IF1503']
     under_turtle = [[inst] for inst in insts_turtle]
     units_turtle = [[1]]*len(insts_turtle)
     
-    insts_daily = ['IF1503']
+    insts_daily = ['IF1503', 'ru1505', 'j1505', 'au1506', 'ag1506', 'RM505','rb1505']
     under_daily = [[inst] for inst in insts_daily]
     units_daily = [[1]]*len(insts_daily)
+    lookbacks_daily = [0]*len(insts_daily)
 
-    dt_strat = strat_dt.DTTrader('DT_test', under_dt, trade_unit = units_dt, agent = None, daily_close = False, email_notify = ['harvey_wwu@hotmail.com'])
-    dt_daily = strat_dt.DTTrader('DT_Daily', under_daily, trade_unit = units_daily, agent = None, daily_close = True, email_notify = ['harvey_wwu@hotmail.com'])
+    dt_strat = strat_dt.DTTrader('DT_test', under_dt, trade_unit = units_dt, lookbacks = lookbacks_dt, agent = None, daily_close = True, email_notify = ['harvey_wwu@hotmail.com'])
+    dt_daily = strat_dt.DTTrader('DT_Daily', under_daily, trade_unit = units_daily, lookbacks = lookbacks_daily, agent = None, daily_close = True, email_notify = ['harvey_wwu@hotmail.com'])
     turtle_strat = strat_turtle.TurtleTrader('Turtle_test', under_turtle, trade_unit = units_turtle, agent=None, email_notify = ['harvey_wwu@hotmail.com'] )
     strategies = [dt_strat, dt_daily, turtle_strat]
     strat_cfg = {'strategies': strategies, \
@@ -65,6 +66,47 @@ def test_main(tday, name='test_trade'):
         myagent.mdapis = [] 
         myagent.trader = None    
 
+def prod_test(tday, name='prod_test'):
+    logging.basicConfig(filename="ctp_" + name + ".log",level=logging.DEBUG,format='%(name)s:%(funcName)s:%(lineno)d:%(asctime)s %(levelname)s %(message)s')
+    #trader_cfg = TEST_TRADER
+    user_cfg = PROD_USER
+    agent_name = name
+    ins_setup = {'m1505':(1, 0.7, 8),
+                'RM505':(0, 0.5, 10),
+                'rb1505':(0,0.5, 10),
+                'y1505':(0, 0.5, 4), 
+                'l1505':(0, 0.5, 4),
+                'pp1505':(0,0.5,4),
+                'ru1505':(2, 0.5, 1),
+                'ag1506':(0, 0.7, 6),
+                'au1506':(0, 0.5, 1),
+                'j1505':(0, 0.5, 2),
+                'al1504':(0, 0.9, 5)}
+    units_dt = [[ins_setup[inst][2]] for inst in ins_setup]
+    under_dt = [[inst] for inst in ins_setup]
+    ratios = [(ins_setup[inst][1], ins_setup[inst][1]) for inst in ins_setup]
+    lookbacks = [ins_setup[inst][0] for inst in ins_setup]
+
+    dt_strat = strat_dt.DTTrader('ProdDT', under_dt, trade_unit = units_dt,
+                                 ratios = ratios, lookbacks = lookbacks, 
+                                 agent = None, daily_close = False, 
+                                 email_notify = ['harvey_wwu@hotmail.com'])
+    strategies = [dt_strat]
+    strat_cfg = {'strategies': strategies, \
+                 'folder': 'C:\\dev\\src\\ktlib\\pythonctp\\pyctp\\', \
+                 'daily_data_days':3, \
+                 'min_data_days':1 }
+    #myagent = create_agent(agent_name, user_cfg, trader_cfg, insts, strat_cfg)
+    all_insts = ins_setup.keys()
+    myagent, my_trader = emulator.create_agent_with_mocktrader(agent_name, all_insts, strat_cfg, tday)
+    fut_api.make_user(myagent,user_cfg)
+    myagent.resume()
+    try:
+        while 1: time.sleep(1)
+    except KeyboardInterrupt:
+        myagent.mdapis = [] 
+        myagent.trader = None    
+        
 if __name__=="__main__":
     args = sys.argv[1:]
     if len(args) < 2:
