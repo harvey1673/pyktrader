09bb08b395e5ce3d77edcf71285e3bd8aeac6a75
harveywwu <harvey_wwu@hotmail.com>
2015-03-17 18:28:07 +0800

    Update gui_example1.py

--- gui_example1.py
+++ gui_example1.py
@@ -1,79 +1,124 @@
-import Tkinter
-import tkFont as font
-
-class Gui(Tkinter.Tk):      
-    def __init__(self, agent):
-        Tkinter.Tk.__init__(self)
-
-        self.logic = logic
-
-        self.geometry("370x170")
-        self.resizable(width=False, height=False)
-
-        rock_button = Tkinter.Button(self, text="Rock", command=self.rock_clicked)
-        rock_button.place(width=100, height=30, x=10, y=30)
-
-        paper_button = Tkinter.Button(self, text="Paper", command=self.paper_clicked)
-        paper_button.place(width=100, height=30, x=10, y=70)
-
-        scissors_button = Tkinter.Button(self, text="Scissors", command=self.scissors_clicked)
-        scissors_button.place(width=100, height=30, x=10, y=110)
-
-        score_font = font.Font(family="Helvetica", size=20)
-
-        own_score_lbl = Tkinter.Label(self, text="0", relief=Tkinter.RIDGE, font=score_font)
-        own_score_lbl.place(width=50, height=110, x=120, y=30)
-
-        ai_score_lbl = Tkinter.Label(self, text="0", relief=Tkinter.RIDGE, font=score_font)
-        ai_score_lbl.place(width=50, height=110, x=200, y=30)
-
-        ai_choice = Tkinter.Label(self, relief=Tkinter.RIDGE)
-        ai_choice.place(width=100, height=110, x=260, y=30)
-
-        self.render_title()
-
-    def render_title(self):
-        logic = self.logic
-        templ = "Rock({logic.rock_counter}), Paper({logic.paper_counter}), Scissors({logic.scissors_counter})"
-        title = templ.format(logic=logic)
-        self.title(title)
-
-    def rock_clicked(self):
-        self.logic.play_rock()
-        self.render_title()
-
-    def paper_clicked(self):
-        self.logic.play_paper()
-        self.render_title()
-
-    def scissors_clicked(self):
-        self.logic.play_scissors()
-        self.render_title()
-
+import agent as agent
+import fut_api
+#import lts_api
+import logging
+import mysqlaccess
+import misc
 import Tkinter as tk
+import threading
+import ScrolledText
+import update_contract_table
+import sys
+import datetime
+import threading
 
-class Demo1:
-    def __init__(self, master):
-        self.master = master
-        self.frame = tk.Frame(self.master)
-        self.button1 = tk.Button(self.frame, text = 'New Window', width = 25, command = self.new_window)
-        self.button1.pack()
-        self.frame.pack()
-    def new_window(self):
-        self.newWindow = tk.Toplevel(self.master)
-        self.app = Demo2(self.newWindow)
+class TextHandler(logging.Handler):
+    """This class allows you to log to a Tkinter Text or ScrolledText widget"""
+    def __init__(self, text):
+        # run the regular Handler __init__
+        logging.Handler.__init__(self)
+        # Store a reference to the Text it will log to
+        self.text = text
+ 
+    def emit(self, record):
+        msg = self.format(record)
+        def append():
+            self.text.configure(state='normal')
+            self.text.insert(tk.END, msg + '\n')
+            self.text.configure(state='disabled')
+            # Autoscroll to the bottom
+            self.text.yview(tk.END)
+        # This is necessary because we can't modify the Text from other threads
+        self.text.after(0, append)
+		
+class GuiApp(tk.Tk):
+    def __init__(self, name='SaveAgent', tday = datetime.date.today()):
+		tk.Tk.__init__(self)
+        self.name = name        
+        app = self.app = tk.Tk()
+        app.title(name)
+        self.scroll_text = ScrolledText.ScrolledText(self.app, state='disabled')
+        self.scroll_text.configure(font='TkFixedFont')
+        # Create textLogger
+        self.text_handler = TextHandler(self.scroll_text)
+        self.scroll_text.pack()
+        self.agent = None
+        self.trader = None
+        self.user = None
+        self.scur_day = tday
+        menu = tk.Menu(self.app)
+        menu.add_command(label="Update contracts", command=self.onUpdateCont)
+        menu.add_command(label="Restart agent", command=self.onRestart)
+        menu.add_command(label="Exit", command=self.onExit)
+        app.config(menu=menu)
 
-class Demo2:
-    def __init__(self, master):
-        self.master = master
-        self.frame = tk.Frame(self.master)
-        self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
-        self.quitButton.pack()
-        self.frame.pack()
-    def close_windows(self):
-        self.master.destroy()
+    def onUpdateCont(self):
+        t = threading.Thread(target=update_contract_table.main)
+        t.start()
 
-def main(): 
-    root = tk.Tk()
-    app = Demo1(root)
-    root.mainloop()
+    def onRestart(self):
+        if self.agent != None:
+            self.scur_day = self.agent.scur_day
+        save_insts = filter_main_cont(self.scur_day)
+        self.agent = agent.SaveAgent(name = self.name, trader = None, cuser = None, instruments=save_insts, daily_data_days=0, min_data_days=0, tday = tday)
+        self.agent.logger.addHandler(self.text_handler)
+        fut_api.make_user(self.agent, misc.PROD_USER)
+        return
+    
+    def onExit(self):
+        if self.agent != None:
+            self.agent.mdapis = []
+            self.agent.trader = None
+        self.app.destroy()
+        return
+
+class MainApp(object):
+	def __init__(self, name, trader_cfg, user_cfg, strat_cfg):
+		self.trader_cfg = trader_cfg
+		self.user_cfg = user_cfg
+		self.name = name
+		self.agent = None
+		self.trader = None
+		self.user= None
+	def restart(self):
+		pass
+	
+	def 
+		
+def restart(tday, name='option_test'):
+
+    logging.basicConfig(filename="ctp_" + name + ".log",level=logging.DEBUG,format='%(name)s:%(funcName)s:%(lineno)d:%(asctime)s %(levelname)s %(message)s')
+    trader_cfg = TEST_TRADER
+    user_cfg = TEST_USER
+    agent_name = name
+    opt_strat = optstrat.IndexFutOptStrat(name, 
+                                    ['IF1503', 'IF1506'], 
+                                    [datetime.datetime(2015, 3, 20, 15, 0, 0), datetime.datetime(2015,6,19,15,0,0)],
+                                    [[3400, 3450, 3500, 3550, 3600, 3650]]*2)
+
+    insts_dt = ['IF1503']
+    units_dt = [1]*len(insts_dt)
+    under_dt = [[inst] for inst in insts_dt]
+    vols_dt = [[1]]*len(insts_dt)
+    lookbacks_dt = [0]*len(insts_dt)
+    
+    insts_daily = ['IF1503']
+    under_daily = [[inst] for inst in insts_daily]
+    vols_daily = [[1]]*len(insts_daily)
+    units_daily = [1]*len(insts_daily)
+    lookbacks_daily = [0]*len(insts_daily)
+
+    dt_strat = strat_dt.DTTrader('DT_test', under_dt, vols_dt, trade_unit = units_dt, lookbacks = lookbacks_dt, agent = None, daily_close = False, email_notify = [])
+    dt_daily = strat_dt.DTTrader('DT_Daily', under_daily, vols_daily, trade_unit = units_daily, lookbacks = lookbacks_daily, agent = None, daily_close = True, email_notify = ['harvey_wwu@hotmail.com'])
+    
+    strategies = [dt_strat, dt_daily, opt_strat]
+    all_insts = opt_strat.instIDs
+    strat_cfg = {'strategies': strategies, \
+                 'folder': 'C:\\dev\\src\\ktlib\\pythonctp\\pyctp\\', \
+                 'daily_data_days':3, \
+                 'min_data_days':1 }
+    #myagent, my_trader = emulator.create_agent_with_mocktrader(agent_name, all_insts, strat_cfg, tday)
+    myagent = fut_api.create_agent(agent_name, user_cfg, trader_cfg, all_insts, strat_cfg, tday)
+    #fut_api.make_user(myagent,user_cfg)
+    myagent.resume()
+    
