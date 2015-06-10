766881e5e2b1da6adcb72b5cba1ae917c6f0e7d3
harveywwu <harvey_wwu@hotmail.com>
2015-06-11 01:54:34 +0800

    update 2

--- optionarb.py
+++ optionarb.py
@@ -9,7 +9,7 @@ def get_option_map(underliers, cont_mths, strikes):
         for otype in ['C', 'P']:
             for strike in ks:
                 #cont_mth = int(under[-4:]) + 200000
-                key = (str(under), cmth, otype, strike)
+                key = (str(under), otype, strike)
                 instID = under
                 if instID[:2] == "IF":
                     instID = instID.replace('IF', 'IO')
@@ -23,7 +23,7 @@ class OptionArbStrat(strategy.Strategy):
         underliers = []
         volumes = []
         trade_units = []
-        self.future_conts = future_conts
+        self.future_conts = future_conts 
         self.cont_mths = cont_mths
         self.strikes = strikes
         self.cp_parity = {}
@@ -32,41 +32,41 @@ class OptionArbStrat(strategy.Strategy):
         self.call_bfly = {}
         self.put_bfly = {}
         idx = 0
-        for fut, cmth, strike_list in zip(future_conts, cont_mths, strikes):
+        for fut, strike_list in zip(future_conts, strikes):
             slen = len(strike_list)
             for i, strike in enumerate(strike_list):
-                call_key = (fut, cmth, 'C', strike)
-                put_key  = (fut, cmth, 'P', strike)
+                call_key = (fut, 'C', strike)
+                put_key  = (fut, 'P', strike)
                 underliers.append([self.option_map[call_key], self.option_map[put_key], fut])
                 volumes.append([3, -3, -1])
                 trade_units.append(1)
                 self.cp_parity[(fut, strike)] = {'idx':idx }
                 idx += 1
                 if (i < slen - 1):
-                    next_call = (fut, cmth, 'C', strike_list[i+1])
+                    next_call = (fut, 'C', strike_list[i+1])
                     underliers.append([self.option_map[call_key], self.option_map[next_call]])
                     volumes.append([1, -1])
                     trade_units.append(1)
-                    self.call_spread[(cmth, strike)] = {'idx':idx }
+                    self.call_spread[(fut, strike)] = {'idx':idx }
                     idx += 1
                     next_put = (fut, 'P', strike_list[i+1])
                     underliers.append([self.option_map[next_put], self.option_map[put_key]])
                     volumes.append([1, -1])
                     trade_units.append(1)
-                    self.put_spread[(cmth, strike)] = {'idx':idx }
+                    self.put_spread[(fut, strike)] = {'idx':idx }
                     idx += 1
                     if i > 0:
-                        prev_call = (fut, cmth, 'C', strike_list[i-1])
+                        prev_call = (fut, 'C', strike_list[i-1])
                         underliers.append([self.option_map[prev_call], self.option_map[call_key], self.option_map[next_call]])
                         volumes.append([1, -2, 1])
                         trade_units.append(1)
-                        self.call_bfly[(cmth, strike)] = {'idx':idx }
+                        self.call_bfly[(fut, strike)] = {'idx':idx }
                         idx += 1
-                        prev_put = (fut, cmth, 'P', strike_list[i-1])
+                        prev_put = (fut, 'P', strike_list[i-1])
                         underliers.append([self.option_map[prev_put], self.option_map[put_key], self.option_map[next_put]])
                         volumes.append([1, -2, 1])
                         trade_units.append(1)
-                        self.put_bfly[(cmth, strike)] = {'idx':idx }
+                        self.put_bfly[(fut, strike)] = {'idx':idx }
                         idx += 1
         strategy.Strategy.__init__(self, name, underliers, volumes, trade_units, agent, rmail_notify = email_notify)
         self.profit_ratio = 0
@@ -84,8 +84,15 @@ class OptionArbStrat(strategy.Strategy):
     def tick_run(self, ctick):
         instID = ctick.instID
         inst = self.agent.instruments[instID]
-        if (inst.ptype == instrument.ProductType.Future) or (inst.ptype == instrument.ProductType.Stock):
-            pass
+        if (inst.ptype == instrument.ProductType.Future):
+            for i, fut in enumerate(self.future_conts):
+                if fut == inst.name:
+                    strike_list = self.strikes[i]
+                    for strike in strike_list:
+                        pass
+                    break
+                
+            
         if self.update_positions(idx):
             self.save_state()
         self.run_tick(idx, ctick)
