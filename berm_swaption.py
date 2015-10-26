2e01b033c285de21c984a75b416f578d1cb4fae5
harveywwu <harvey_wwu@hotmail.com>
2015-10-26 19:41:30 +0800

    Update berm_swaption.py

--- berm_swaption.py
+++ berm_swaption.py
@@ -2,14 +2,14 @@ from QuantLib import *
 
 # global data
 calendar = UnitedStates()
-todaysDate = Date(15, 10, 2015);
+todaysDate = Date(27, 10, 2015);
 Settings.instance().evaluationDate = todaysDate
-settlementDate = Date(19, 10, 2015);
+settlementDate = Date(29, 10, 2015);
 
 # market quotes
-deposits = { (1,Weeks):  0.002749,
-             (1,Months): 0.002881,
-             (2,Months): 0.002983 } 
+deposits = { (1,Weeks):  0.00259231,
+             (1,Months): 0.00280159,
+             (2,Months): 0.00289735 } 
 #             (6,Months): 0.0353,
 #             (9,Months): 0.0348,
 #             (1,Years): 0.0345 }
@@ -18,31 +18,31 @@ deposits = { (1,Weeks):  0.002749,
 #         (6,9): 0.037125,
 #         (9,12): 0.037125 }
 
-futures = { Date(16,12,2015): 99.6375,
-            Date(16, 3,2016): 99.5575,
-            Date(15, 6,2016): 99.4575,
-            Date(21, 9,2016): 99.3325,
-            Date(21,12,2016): 99.1875,
-            Date(15, 3,2017): 99.0675,
-            Date(21, 6,2017): 98.9375,
-            Date(20, 9,2017): 98.8175,
-            Date(20,12,2017): 98.6875,
-            Date(21, 3,2018): 98.5775,
-            Date(20, 6,2018): 98.4675,
-            Date(19, 9,2018): 98.3675 }
+futures = { Date(16,12,2015): 99.6175,
+            Date(16, 3,2016): 99.5125,
+            Date(15, 6,2016): 99.3925,
+            Date(21, 9,2016): 99.2475,
+            Date(21,12,2016): 99.0975,
+            Date(15, 3,2017): 98.9675,
+            Date(21, 6,2017): 98.8275,
+            Date(20, 9,2017): 98.7025,
+            Date(20,12,2017): 98.57,
+            Date(21, 3,2018): 98.4575,
+            Date(20, 6,2018): 98.3475,
+            Date(19, 9,2018): 98.2425 }
 
-swaps = { (4,Years): 0.0114274,
-          (5,Years): 0.0132988,
-          (6,Years): 0.0149687,
-          (7,Years): 0.0163994,
-		  (8,Years): 0.0176033,
-		  (9,Years): 0.0186200,
-		  (10,Years):0.0194827,
-          (12,Years):0.0202205,
-          (15,Years):0.0223418,
-          (20,Years):0.0237100,
-          (25,Years):0.0243868,
-          (30,Years):0.0248008 }
+swaps = { (4,Years): 0.0123356,
+          (5,Years): 0.0142700,
+          (6,Years): 0.0159304,
+          (7,Years): 0.0173300,
+		  (8,Years): 0.0184974,
+		  (9,Years): 0.0194652,
+		  (10,Years):0.0203015,
+          (12,Years):0.0216600,
+          (15,Years):0.0230245,
+          (20,Years):0.0243405,
+          (25,Years):0.0249750,
+          (30,Years):0.0253399, }
 
 # convert them to Quote objects
 for n,unit in deposits.keys():
@@ -114,7 +114,7 @@ depoFuturesSwapCurve = PiecewiseFlatForward(settlementDate, helpers,
 swapEngine = DiscountingSwapEngine(discountTermStructure)
 discountTermStructure.linkTo(depoFuturesSwapCurve)
 swapType = VanillaSwap.Payer
-nominal = 1000
+nominal = 100
 for_len = 1
 to_len = 5
 maturity = calendar.advance(settlementDate, for_len+to_len, Years)
@@ -123,7 +123,7 @@ payFixed = True
 fixedLegFrequency = Semiannual
 fixedLegAdjustment = Unadjusted
 fixedLegDayCounter = Thirty360()
-strike = 0.0205
+strike = 0.015
 
 floatingLegFrequency = Quarterly
 spread = 0.0
@@ -146,7 +146,7 @@ underlying = VanillaSwap(swapType, nominal,
                    floatingSchedule, index, spread,
                    floatingLegDayCounter)
 underlying.setPricingEngine(swapEngine)
-print "spot = %s" % (underlying.fairRate())
+print "spot = %s" % (underlying.NPV())
 #forwardStart = calendar.advance(settlementDate,for_len, Years)
 #forwardEnd = calendar.advance(forwardStart, to_len,Years)
 #fixedSchedule = Schedule(forwardStart, forwardEnd,
@@ -164,33 +164,33 @@ print "spot = %s" % (underlying.fairRate())
 #                      floatingLegDayCounter)
 #underlying.setPricingEngine(swapEngine)
 endDate = floatingSchedule[-1]
-bermudanDates = [ d for d in floatingSchedule ][4:-1]
+bermudanDates = [ calendar.advance(d, -Period(2, Days)) for (idx, d) in enumerate(floatingSchedule) if idx >= 4 and idx % 4 == 0 ]
 print bermudanDates
 exercise = BermudanExercise(bermudanDates, False)
 target_swaption = Swaption(underlying, exercise)
-vv = 0.46
 #swaptionVols = [ (d, endDate, vv) for d in bermudanDates ]
 swaptionVols = [ # maturity,          length,             volatility
-                 (Period(12, Months), Period(60, Months), vv),
-				 (Period(15, Months), Period(57, Months), vv),
-				 (Period(18, Months), Period(54, Months), vv),
-				 (Period(21, Months), Period(51, Months), vv),
-				 (Period(24, Months), Period(48, Months), vv),
-				 (Period(27, Months), Period(45, Months), vv),
-				 (Period(30, Months), Period(42, Months), vv),
-				 (Period(33, Months), Period(39, Months), vv),
-				 (Period(36, Months), Period(36, Months), vv),
-				 (Period(39, Months), Period(33, Months), vv),
-				 (Period(42, Months), Period(30, Months), vv),
-				 (Period(45, Months), Period(27, Months), vv),
-				 (Period(48, Months), Period(24, Months), vv),
-				 (Period(51, Months), Period(21, Months), vv),
-				 (Period(54, Months), Period(18, Months), vv),
-                 (Period(57, Months), Period(15, Months), vv),
-                 (Period(60, Months), Period(12, Months), vv),
-                 (Period(63, Months), Period( 9, Months), vv),
-				 (Period(66, Months), Period( 6, Months), vv),
-                 (Period(69, Months), Period( 3, Months), vv) ]
+                 (Period(12, Months), Period(60, Months), 0.49344),
+				 (Period(24, Months), Period(48, Months), 0.48934),
+				 (Period(36, Months), Period(36, Months), 0.51422),
+				 (Period(48, Months), Period(24, Months), 0.50601),
+				 (Period(60, Months), Period(12, Months), 0.49515),
+				 #(Period(27, Months), Period(45, Months), vv),
+				 #(Period(30, Months), Period(42, Months), vv),
+				 #(Period(33, Months), Period(39, Months), vv),
+				 #(Period(36, Months), Period(36, Months), vv),
+				 #(Period(39, Months), Period(33, Months), vv),
+				 #(Period(42, Months), Period(30, Months), vv),
+				 #(Period(45, Months), Period(27, Months), vv),
+				 #(Period(48, Months), Period(24, Months), vv),
+				 #(Period(51, Months), Period(21, Months), vv),
+				 #(Period(54, Months), Period(18, Months), vv),
+                 #(Period(57, Months), Period(15, Months), vv),
+                 #(Period(60, Months), Period(12, Months), vv),
+                 #(Period(63, Months), Period( 9, Months), vv),
+				 #(Period(66, Months), Period( 6, Months), vv),
+                 #(Period(69, Months), Period( 3, Months), vv),
+				 ]
 helpers = [ SwaptionHelper(exerciseDate, endDate, \
                            QuoteHandle(SimpleQuote(vol)), \
                            index, index.tenor(), index.dayCounter(), \
@@ -199,12 +199,11 @@ helpers = [ SwaptionHelper(exerciseDate, endDate, \
 #volQuote= SimpleQuote(vv)
 #swaptionVol = ConstantSwaptionVolatility(0, UnitedStates(), ModifiedFollowing, volQuote, Actual365Fixed())
 stepDates = bermudanDates[:-1]
-sigmas = [QuoteHandle(SimpleQuote(0.1))]*(len(stepDates)+1)
+sigmas = [QuoteHandle(SimpleQuote(0.01))]*(len(stepDates)+1)
 reversion = [QuoteHandle(SimpleQuote(0.01))]
 gsr = Gsr(discountTermStructure, stepDates, sigmas, reversion, 20.0)
 swaptionEngine = Gaussian1dSwaptionEngine(gsr, 64, 7.0, True,False, discountTermStructure)
-#nonstandardSwaptionEngine =Gaussian1dNonstandardSwaptionEngine( gsr, 64, 7.0, True, False, Quote(), discountTermStructure)
-target_swaption.setPricingEngine(swaptionEngine)
+#nonstandardSwaptionEngine =Gaussian1dNonstandardSwaptionEngine( gsr, 64, 7.0, True, False, QuoteHandle(SimpleQuote(0.0)), discountTermStructure)
 #swapBase = SwapIndex( "UsdLiborSwapIsdaFixAm", 6 * Years, USDCurrency(), TARGET(), 6*Months, ModifiedFollowing, Thirty360(),
 #                      index, discountTermStructure)
 #basket = target_swaption.calibrationBasket(swapBase, swaptionVol)
@@ -214,8 +213,7 @@ for h in helpers:
 	h.setPricingEngine(swaptionEngine)
 
 method = LevenbergMarquardt()
-ec = EndCriteria(1000, 10, 1E-5, 1E-5, 1E-8)
+ec = EndCriteria(1000, 10, 1E-8, 1E-8, 1E-8)
 gsr.calibrateVolatilitiesIterative(helpers, method, ec)
-
+target_swaption.setPricingEngine(swaptionEngine)
 print target_swaption.NPV()
-
